ee7e5bef8992896ee99cf6d3b7d498f6d1a09aee
Jeremy Grosser <jeremy@synack.me>
2019-08-27 13:01:29 -0700

    Use booleans instead of integers for C interface bool

--- modules/free_fall.adb
+++ modules/free_fall.adb
@@ -107,7 +107,7 @@ is
 
       --  Try to detect landing only if a free fall has
       --  been detected and we still are in recovery mode.
-      if In_Recovery = 1 then
+      if In_Recovery = True then
 
          Derivative := Calculate_Last_Derivative (Landing_Data_Collector);
          --  If the derivative between two samples of the Z acceleration
@@ -131,8 +131,8 @@ is
    begin
       --  if the drone is in recovery mode and it has not recovered after
       --  the specified timeout, disable the free fall mode in emergency.
-      if In_Recovery = 1 and then Time_Since_Last_FF > RECOVERY_TIMEOUT then
-         In_Recovery := 0;
+      if In_Recovery = True and then Time_Since_Last_FF > RECOVERY_TIMEOUT then
+         In_Recovery := False;
          FF_Mode := DISABLED;
       end if;
    end FF_Watchdog;
@@ -150,7 +150,7 @@ is
    begin
       --  Check if FF Detection is disabled
       if FF_Mode = DISABLED then
-         In_Recovery := 0;
+         In_Recovery := False;
          return;
       end if;
 
@@ -163,17 +163,17 @@ is
 
       if Has_Landed then
          Last_Landing_Time := Clock;
-         In_Recovery := 0;
+         In_Recovery := False;
       end if;
 
       --  Detect if the drone is in free fall.
       FF_Detect_Free_Fall (Acc, Has_Detected_FF);
 
-      if In_Recovery = 0 and Has_Detected_FF and
+      if In_Recovery = False and Has_Detected_FF and
         Time_Since_Last_Landing > STABILIZATION_PERIOD_AFTER_LANDING
       then
          Last_FF_Detected_Time := Clock;
-         In_Recovery := 1;
+         In_Recovery := True;
          Recovery_Thrust := MAX_RECOVERY_THRUST;
       end if;
 
@@ -191,7 +191,7 @@ is
       Pitch_Type          : in out RPY_Type) is
    begin
       --  If not in recovery, keep the original commands
-      if In_Recovery = 0 then
+      if In_Recovery = False then
          return;
       end if;
 
@@ -214,8 +214,8 @@ is
       --  If not in recovery, keep the original thrust
       --  If the pilot has moved his joystick, the drone is not in recovery
       --  anymore
-      if In_Recovery = 0 or Thrust > 0 then
-         In_Recovery := 0;
+      if In_Recovery = False or Thrust > 0 then
+         In_Recovery := False;
          return;
       end if;
 
